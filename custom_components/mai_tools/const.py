db669e6dfa7f38cce13590f7b16fed1f9e7a6264
Manh Vu <manhvuarchitect@gmail.com>
2026-06-10 16:03:52 +0700

    chore: bump version to v0.0.4

--- custom_components/mai_tools/const.py
+++ custom_components/mai_tools/const.py
@@ -1,6 +1,6 @@
 """Constants for M.A.I Tools."""
 DOMAIN = "mai_tools"
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 BACKUP_FILE_VERSION = "1.0"
 HISTORY_STORAGE_KEY = "mai_tools_history"
 MAX_HISTORY = None  # None = unlimited, user deletes manually
